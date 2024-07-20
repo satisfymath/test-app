5dd22833d97421a33adb0e534d7f9aeb3dfb0faa
david <daviddelmac@gmail.com>
2024-07-20 01:40:54 -0400

    comits

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import Table from './Table'; // Importa el componente Table
-import './App.css'; // Incluye los estilos aquí
+import Table from './Table';
+import './App.css';
+import jsonData from './tables.json';  // Ajusta la ruta según la ubicación del archivo
 
 const App = () => {
-  const [jsonData, setJsonData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    const loadJson = async () => {
-      try {
-        const response = await fetch('/tables.json');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setJsonData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    loadJson();
+    // Aquí podrías hacer algún procesamiento adicional si es necesario
+    setData(jsonData);
   }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div id="tables-container">
-      {Object.keys(jsonData).map(key => (
-        <Table key={key} title={key} data={jsonData[key]} />
-      ))}
+      {Object.keys(data).length > 0 ? (
+        Object.keys(data).map(key => (
+          <Table key={key} title={key} data={data[key]} />
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
     </div>
   );
 };
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import names from './Names.json'; // Ajusta la ruta según tu estructura
+//import names from './Names.json'; // Ajusta la ruta según tu estructura
+
+const names  = {
+  "PEF05":"Peso por edad en niñas desde el nacimiento a 5 años",
+  "PEM05": "Peso por edad en niños desde el nacimiento a 5 años",
+  "TEF05": "Longitud/estatura por edad en niñas desde el nacimientos a 5 años",
+  "TEM05": "Longitud/estatura por edad en niños desde el nacimientos a 5 años",
+  "PTF02": "Peso por longitud en niñas desde el nacimiento a 2 años, de 45 cm a 110 cm",
+  "PTM02": "Peso por longitud en niños desde el nacimiento a 2 años, de 45 cm a 110 cm",
+  "PTF25": "Peso por estatura en niñas de 2 años a 5 años, de 65 cm a 120 cm",
+  "PTM25": "Peso por estatura en niños de 2 años a 5 años, de 65 cm a 120 cm",
+  "PEF510": "Peso por edad en niñas de 5 años 1 mes a 10 años",
+  "PEM510": "Peso por edad en niñas de 5 años 1 mes a 10 años",
+  "TEF519": "Estatura por edad en niñas de 5 años 1 mes a 19 años",
+  "TEM519": "Estatura por edad en niños de 5 años 1 mes a 19 años",
+  "IMCF519": "IMC por edad en niñas y adolescentes de 5 años 1 mes a 19 años",
+  "IMCM519": "IMC por edad en niños y adolescentes de 5 años 1 mes a 19 años"
+}
 
 const Table = ({ title, data }) => {
-  if (!data || Object.keys(data).length === 0) return null;
+  if (!data || Object.keys(data).length === 0) return <p>No data available</p>;
 
   // Función para redondear números
   const formatNumber = (value) => {
-    // Verifica si el valor es un número y lo redondea a 2 decimales
     return typeof value === 'number' ? value.toFixed(2) : value;
   };
 
+  // Obtén los encabezados de la primera entrada de data
   const headers = Object.keys(data[Object.keys(data)[0]]);
+  
+  // Crea las filas de la tabla
   const rows = Object.keys(data).map(rowKey => (
     <tr key={rowKey}>
       {Object.values(data[rowKey]).map((value, index) => (
@@ -21,10 +40,12 @@ const Table = ({ title, data }) => {
 
   // Obtén la descripción del título usando el archivo JSON
   const tableTitle = names[title] || 'Título no disponible';
+  console.log('Table Title:', tableTitle);
 
   // Determina la clase para el título y la tabla
   const titleClass = tableTitle.toLowerCase().includes('niñas') ? 'ninas' :
                      tableTitle.toLowerCase().includes('niños') ? 'ninos' : '';
+  console.log('Title Class:', titleClass);
 
   return (
     <div>
