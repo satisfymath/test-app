f231dde44b6a249ff932ab0789bbe134909623fc
david <daviddelmac@gmail.com>
2024-07-24 01:58:00 -0400

    update

--- src/EVNutricional.js
+++ src/EVNutricional.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-//import tables from './tables.json';
+import tables from './tables.json';
 
 const tablaDescripciones = {
   "PEF05": "Peso por edad en niñas desde el nacimiento a 5 años",
@@ -18,6 +18,53 @@ const tablaDescripciones = {
   "IMCM519": "IMC por edad en niños y adolescentes de 5 años 1 mes a 19 años"
 };
 
+const classifyValue = (record, value) => {
+  if (!record) {
+    return "No hay datos para la clasificación.";
+  }
+
+  if (value <= record["-2DE"]) {
+    return "-2DE";
+  } else if (value > record["-2DE"] && value <= record["-1DE"]) {
+    return "-1DE";
+  } else if (value > record["-1DE"] && value < record["+1DE"]) {
+    return "Normal";
+  } else if (value >= record["+1DE"] && value < record["+2DE"]) {
+    return "+1DE";
+  } else if (value >= record["+2DE"]) {
+    return value >= record["+3DE"] ? "+3DE" : "+2DE";
+  }
+
+  return "Valor no clasificado.";
+};
+
+const calcularEdadCronologica = (fechaNacimiento) => {
+  if (!fechaNacimiento) return '';
+
+  const fechaActual = new Date();
+  const fechaNacimientoDate = new Date(fechaNacimiento);
+
+  const años = fechaActual.getFullYear() - fechaNacimientoDate.getFullYear();
+  const meses = fechaActual.getMonth() - fechaNacimientoDate.getMonth();
+  const dias = fechaActual.getDate() - fechaNacimientoDate.getDate();
+
+  let edadAños = años;
+  let edadMeses = meses;
+  let edadDias = dias;
+
+  if (edadDias < 0) {
+    edadMeses--;
+    edadDias += new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 0).getDate();
+  }
+
+  if (edadMeses < 0) {
+    edadAños--;
+    edadMeses += 12;
+  }
+
+  return `${edadAños} años ${edadMeses} meses ${edadDias} días`;
+};
+
 const EVNutricional = () => {
   const [nombre, setNombre] = useState('');
   const [fechaNacimiento, setFechaNacimiento] = useState('');
@@ -25,186 +72,164 @@ const EVNutricional = () => {
   const [peso, setPeso] = useState('');
   const [talla, setTalla] = useState('');
 
-  // Calcula la edad en meses a partir de la fecha de nacimiento
   const calcularEdadEnMeses = () => {
     if (fechaNacimiento) {
       const fechaActual = new Date();
       const fechaNacimientoDate = new Date(fechaNacimiento);
-      const edadEnMeses = Math.floor((fechaActual - fechaNacimientoDate) / (1000 * 60 * 60 * 24 * 30));
-      return edadEnMeses;
+
+      const diferenciaMs = fechaActual - fechaNacimientoDate;
+      const diferenciaMeses = diferenciaMs / (1000 * 60 * 60 * 24 * 30.436875);
+      const edadMeses = Math.floor(diferenciaMeses);
+      const diferenciaDias = (diferenciaMeses - edadMeses) * 30.436875;
+      const edadDias = Math.floor(diferenciaDias);
+
+      if (edadDias >= 1 && edadDias <= 15) {
+        return edadMeses;
+      } else if (edadDias >= 16) {
+        return edadMeses + 1;
+      }
     }
     return null;
   };
 
-// Obtiene las claves de las tablas en función de la edad y el sexo
-const obtenerClavesTablas = () => {
+  const obtenerClavesTablas = () => {
     const edadEnMeses = calcularEdadEnMeses();
     const clavesTablas = [];
     if (edadEnMeses !== null) {
-      if (edadEnMeses < 24) { // Menor a 2 años
+      if (edadEnMeses < 24) {
         if (sexo === 'Femenino') {
-          clavesTablas.push('PTF02'); // Peso por longitud para niñas de 0 a 2 años
-          clavesTablas.push('PTF02'); // Peso por longitud para niñas de 0 a 2 años
-          clavesTablas.push('TEF05'); // longitud por edad para niñas desde el nacimiento a 5 años
-        } else { // Masculino
-          clavesTablas.push('PTM02'); // Peso por longitud para niños de 0 a 2 años
-          clavesTablas.push('PEM05'); // Peso por edad para niños desde el nacimiento a 5 años
-          clavesTablas.push('TEM05'); // longitud por edad para niños desde el nacimiento a 5 años
-        }
-      } else if (edadEnMeses < 60) { // De 2 a 5 años
-        if (sexo === 'Femenino') {
-          clavesTablas.push('PTF25'); // Peso por estatura para niñas de 2 a 5 años
-          clavesTablas.push('PEF05'); // Peso por edad para niñas desde el nacimiento a 5 años
-          clavesTablas.push('TEF05'); // longitud por edad para niñas desde el nacimiento a 5 años
-        } else { // Masculino
-          clavesTablas.push('PTM25'); // Peso por estatura para niños de 2 a 5 años
-          clavesTablas.push('PEM05'); // Peso por edad para niños desde el nacimiento a 5 años
-          clavesTablas.push('TEM05'); // longitud por edad para niñas desde el nacimiento a 5 años
-        }
-      } else if (edadEnMeses <= 120) { // De 5 años 1 mes a 10 años
-        if (sexo === 'Femenino') {
-            clavesTablas.push('PEF510');
-            clavesTablas.push('IMCF519'); // IMC por edad para niñas de 5 años 1 mes a 19 años
-            clavesTablas.push('TEF519'); // Estatura para niñas de 5 años 1 mes a 19 años // Peso por edad para niñas de 5 años 1 mes a 10 años
-        } else {//Masculino
-            clavesTablas.push('PEM510'); // Peso por edad para niños de 5 años 1 mes a 10 años
-            clavesTablas.push('IMCM519'); // IMC por edad para niños de 5 años 1 mes a 19 años
-            clavesTablas.push('TEM519'); // Estatura para niños de 5 años 1 mes a 19 años
-        } 
-      } else { // De 5 años 1 mes a 19 años
-        if (sexo === 'Femenino') {
-          clavesTablas.push('IMCF519'); // IMC por edad para niñas de 5 años 1 mes a 19 años
-          clavesTablas.push('TEF519'); // Estatura para niñas de 5 años 1 mes a 19 años
+          clavesTablas.push('PTF02', 'PEF05', 'TEF05');
         } else {
-          clavesTablas.push('IMCM519'); // IMC por edad para niños de 5 años 1 mes a 19 años
-          clavesTablas.push('TEM519'); // Estatura para niños de 5 años 1 mes a 19 años
+          clavesTablas.push('PTM02', 'PEM05', 'TEM05');
+        }
+      } else if (edadEnMeses < 60) {
+        if (sexo === 'Femenino') {
+          clavesTablas.push('PTF25', 'PEF05', 'TEF05');
+        } else {
+          clavesTablas.push('PTM25', 'PEM05', 'TEM05');
+        }
+      } else if (edadEnMeses <= 120) {
+        if (sexo === 'Femenino') {
+          clavesTablas.push('PEF510', 'IMCF519', 'TEF519');
+        } else {
+          clavesTablas.push('PEM510', 'IMCM519', 'TEM519');
+        }
+      } else {
+        if (sexo === 'Femenino') {
+          clavesTablas.push('IMCF519', 'TEF519');
+        } else {
+          clavesTablas.push('IMCM519', 'TEM519');
         }
       }
     }
     return clavesTablas;
   };
 
-
-  // Obtiene la descripción de las tablas en función de las claves de las tablas
   const obtenerDescripcionesTablas = (clavesTablas) => {
-    return clavesTablas.map(clave => tablaDescripciones[clave] || 'Descripción no disponible');
+    return clavesTablas.map(clave => `${tablaDescripciones[clave]}: ${EvaluacionNutricional(clave)}` || 'Descripción no disponible');
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const EvaluacionNutricional = (key) => {
+    const edadEnMeses = calcularEdadEnMeses();
+    const pesoNum = parseFloat(peso);
+    const tallaNum = parseFloat(talla);
 
-    // Define el objeto con los datos del formulario
-    const formData = {
-      nombre,
-      fechaNacimiento,
-      sexo,
-      peso,
-      talla,
-    };
-
-    // Obtén los datos existentes en el localStorage
-    const existingData = JSON.parse(localStorage.getItem('formData')) || [];
-
-    // Añade los nuevos datos al array existente
-    existingData.push(formData);
-
-    // Guarda el array actualizado en localStorage
-    localStorage.setItem('formData', JSON.stringify(existingData));
-
-    // Limpia los campos del formulario
-    setNombre('');
-    setFechaNacimiento('');
-    setSexo('');
-    setPeso('');
-    setTalla('');
-
-    console.log('Datos guardados en localStorage:', existingData);
+    if (key.includes('PE')) {
+      const record = tables[key] ? tables[key][edadEnMeses.toFixed(1)] : null;
+      return classifyValue(record, pesoNum);
+    } else if (key.includes('PT')) {
+      const record = tables[key] ? tables[key][tallaNum.toFixed(1)] : null;
+      return classifyValue(record, peso);
+    } else if (key.includes('TE')) {
+      const record = tables[key] ? tables[key][edadEnMeses.toFixed(1)] : null;
+      return classifyValue(record, tallaNum);
+    } else if (key.includes('IMC')) {
+      const imc = calcularIMC();
+      const record = tables[key] ? tables[key][edadEnMeses.toFixed(1)] : null;
+      return classifyValue(record, imc);
+    }
+    return '';
   };
 
-  // Calcular IMC
   const calcularIMC = () => {
     if (peso && talla) {
-      const tallaEnMetros = talla / 100; // Convertir la talla a metros
-      return (peso / (tallaEnMetros ** 2)).toFixed(2); // Calcular IMC y limitar a 2 decimales
+      const tallaEnMetros = talla / 100;
+      return (peso / (tallaEnMetros ** 2)).toFixed(2);
     }
     return null;
   };
 
   const imc = calcularIMC();
-
-  // Obtener las claves de las tablas en función del sexo y la edad
   const clavesTablas = obtenerClavesTablas();
   const descripcionesTablas = obtenerDescripcionesTablas(clavesTablas);
+  const edadCronologica = calcularEdadCronologica(fechaNacimiento);
 
   return (
     <div>
       <h1>Evaluación Nutricional</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h2>Datos Paciente</h2>
-          <label>Nombre:</label>
-          <input 
-            type="text" 
-            value={nombre} 
-            onChange={(e) => setNombre(e.target.value)} 
-          />
-        </div>
-        <div>
-          <label>Fecha de Nacimiento:</label>
-          <input 
-            type="date" 
-            value={fechaNacimiento} 
-            onChange={(e) => setFechaNacimiento(e.target.value)} 
-          />
-        </div>
-        <div>
-          <label>Sexo:</label>
-          <select value={sexo} onChange={(e) => setSexo(e.target.value)}>
-            <option value="">Selecciona una opción</option>
-            <option value="Masculino">Masculino</option>
-            <option value="Femenino">Femenino</option>
-          </select>
-        </div>
-        <div>
-          <label>Peso (kg):</label>
-          <input 
-            type="number" 
-            value={peso} 
-            onChange={(e) => setPeso(e.target.value)} 
-          />
-        </div>
-        <div>
-          <label>Talla (cm):</label>
-          <input 
-            type="number" 
-            value={talla} 
-            onChange={(e) => setTalla(e.target.value)} 
-          />
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
       <div>
-        <h2>Evaluación del Paciente</h2>
-        {imc !== null ? (
-          <>
-            <p>IMC: {imc}</p>
-            {descripcionesTablas.length > 0 ? (
-              <div>
-                <h3>Tablas Correspondientes:</h3>
-                <ul>
-                  {descripcionesTablas.map((descripcion, index) => (
-                    <li key={index}>{descripcion}</li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              <p>No se encontró información para la edad en meses.</p>
-            )}
-          </>
-        ) : (
-          <p>Introduce el peso y la talla para calcular el IMC.</p>
-        )}
+        <h2>Datos Paciente</h2>
+        <label>Nombre:</label>
+        <input
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
       </div>
+      <div>
+        <label>Fecha de Nacimiento:</label>
+        <input
+          type="date"
+          placeholder="dd-mm-yyyy"
+          value={fechaNacimiento}
+          onChange={(e) => setFechaNacimiento(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Sexo:</label>
+        <select value={sexo} onChange={(e) => setSexo(e.target.value)}>
+          <option value="">Seleccionar...</option>
+          <option value="Femenino">Femenino</option>
+          <option value="Masculino">Masculino</option>
+        </select>
+      </div>
+      <div>
+        <label>Peso (kg):</label>
+        <input
+          type="number"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Talla (cm):</label>
+        <input
+          type="number"
+          value={talla}
+          onChange={(e) => setTalla(e.target.value)}
+        />
+      </div>
+      <h2>Evaluación del Paciente</h2>
+      {imc !== null ? (
+        <>
+          <p>Edad Cronológica: {edadCronologica}</p>
+          <p>IMC: {imc}</p>
+          {descripcionesTablas.length > 0 ? (
+            <div>
+              <h3>Descripciones de Tablas</h3>
+              <ul>
+                {descripcionesTablas.map((descripcion, index) => (
+                  <li key={index}>{descripcion}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No hay descripciones disponibles para mostrar.</p>
+          )}
+        </>
+      ) : (
+        <p>No se ha calculado el IMC aún.</p>
+      )}
     </div>
   );
 };
